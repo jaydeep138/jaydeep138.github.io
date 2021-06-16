27404e26c2a360207698ba54d7a218f5d491a590
Jaydeep <jaydeepmulani123@gmail.com>
2021-06-16 23:38:01 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let themeDots=document.getElementsByClassName('theme-dot')
+for(var i=0;i<themeDots.length;i++)
+{
+    themeDots[i].addEventListener('click',function(){
+        let mode=this.dataset.mode;
+        // console.log(mode);
+        setTheme(mode)
+    })
+
+}
+function setTheme(mode){
+    if(mode=='light'){
+        document.getElementById('theme-style').href='default.css';
+    }
+    if(mode=='blue'){
+        document.getElementById('theme-style').href='blue.css';
+    }
+    if(mode=='green'){
+        document.getElementById('theme-style').href='green.css';
+    }
+    if(mode=='purple'){
+        document.getElementById('theme-style').href='purple.css';
+    }
+}
